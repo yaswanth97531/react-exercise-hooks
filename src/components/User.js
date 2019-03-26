643caed478aa4e77b8b478066268043a834c56e3
sabrah11 <runga.abraham@ampf.com>
2019-03-26 10:21:33 -0500

    adding more refactoring and state mutation opportunity for refactor

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
 class User extends React.Component {
-  render() {
-    return <li>{this.props.user.name}</li>
+  constructor (props) {
+    super(props)
+    this.state = {
+      isOpen: false
+    }
+    this.showCity = this.showCity.bind(this)
+  }
+
+  showCity () {
+    this.state.isOpen = !this.state.isOpen // directly mutating the state
+  }
+
+  render () {
+    return (
+      <div>
+        <li title='Click to display city' onClick={this.showCity}>
+          {this.props.user.name}
+          {this.state.isOpen && (
+            <span style={{ fontWeight: 'bold' }}> - {this.props.user.address.city} </span>
+          )}
+        </li>
+      </div>
+    )
   }
 }
 
